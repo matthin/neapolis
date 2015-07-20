71412f4986fa9cb3c9989461853b8065ea75e8b2
Justin Harrison <justin@matthin.com>
2015-07-19 20:37:12 -0400

    Detect successfulness only when needed

--- include/neapolis/Response.h
+++ include/neapolis/Response.h
@@ -7,14 +7,12 @@ namespace nea {
 class Response {
 public:
   Response(const short code, const std::string& text)
-      : code(code), text(std::move(text)), successful(detectSuccess(code)) {}
+      : code(code), text(std::move(text)) {}
 
   const short code;
   const std::string text;
-  const bool successful;
 
-private:
-  bool detectSuccess(const short code) {
+  bool successful() {
     // Neocity only returns 200 on success. It's kind of dumb, but whatever.
     return code == 200;
   }
